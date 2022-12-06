c3a2c79acfd28f161e8b801df30ada2bb2cb4722
ajit2599 <godeajit71@gmail.com>
2022-12-06 18:33:05 +0530

    add

--- cypress/integration/Login/login_spec.js
+++ cypress/integration/Login/login_spec.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Verify the login functionality',()=>{
+    let baseUrl = Cypress.config().baseUrl
     it.only('verify login',()=>{
       // cy.visit('/Datepicker/index.html')
-     let baseUrl = Cypress.config().baseUrl
+     
      cy.visit(baseUrl)
     })
     it('verify login',()=>{
-        cy.visit('/Login-Portal/index.html')
+        cy.visit(baseUrl)
      })
 })
